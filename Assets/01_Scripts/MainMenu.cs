05b0b26f8105716ff660734b65c2445d78a3e2a9
Cabragamer05 <sebastiancabreragomez@gmail.com>
2025-10-02 23:20:44 -0400

    MAIN MENU - JUEGO
    terminar main menu
    y unir el ball logic con el main menu

--- Assets/01_Scripts/MainMenu.cs
+++ Assets/01_Scripts/MainMenu.cs
@@ -1,10 +1,20 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private string gameSceneName = "GameScene";
+    [SerializeField] private TextMeshProUGUI highScoreText; 
 
+    private void Start()
+    {
+        if (highScoreText != null)
+        {
+            int hi = PlayerPrefs.GetInt("HighScore", 0);
+            highScoreText.text = $"High Score: {hi}";
+        }
+    }
     public void StartGame()
     {
         SceneManager.LoadScene(gameSceneName);
